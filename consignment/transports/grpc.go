a1a9877fa3cdf7942355b3a0ddeadcbf9fdf3539
Cristopher <cristophernamchee12@gmail.com>
2021-08-08 21:21:19 +0700

    :sparkles: feat: Add transport layer logic

--- consignment/transports/grpc.go
+++ consignment/transports/grpc.go
@@ -2,8 +2,8 @@ package transport
 
 import (
 	"context"
-	"log"
 	"errors"
+	"log"
 
 	endpoints "github.com/Namchee/microservice-tutorial/consignment/endpoints"
 	"github.com/Namchee/microservice-tutorial/consignment/pb"
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	assertionError: error
+	errAssertion = errors.New("failed to assert input type")
 )
 
 type gRPCServer struct {
@@ -23,17 +23,73 @@ func NewGRPCServer(endpoints endpoints.Endpoints, logger log.Logger) pb.Consignm
 	return &gRPCServer{
 		createConsignment: gt.NewServer(
 			endpoints.CreateConsignment,
-
-		)
+			decodeCreateConsignmentRequest,
+			encodeCreateConsignmentResponse,
+		),
+		getAll: gt.NewServer(
+			endpoints.GetAll,
+			decodeGetAllRequest,
+			encodeGetAllResponse,
+		),
 	}
+}
+
+func (svr *gRPCServer) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	_, res, err := svr.createConsignment.ServeGRPC(ctx, req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res.(*pb.Response), nil
+}
+
+func (svr *gRPCServer) GetAll(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	_, res, err := svr.getAll.ServeGRPC(ctx, req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res.(*pb.Response), nil
 }
 
 func decodeCreateConsignmentRequest(_ context.Context, request interface{}) (interface{}, error) {
 	req, err := request.(*pb.Consignment)
-	
-	if err != nil {
-		return nil, 
+
+	if err {
+		return nil, errAssertion
 	}
 
-	return 
+	return req, nil
+}
+
+func encodeCreateConsignmentResponse(_ context.Context, response interface{}) (interface{}, error) {
+	res, err := response.(*pb.Response)
+
+	if err {
+		return nil, errAssertion
+	}
+
+	return res, nil
+}
+
+func decodeGetAllRequest(_ context.Context, request interface{}) (interface{}, error) {
+	req, err := request.(*pb.GetRequest)
+
+	if err {
+		return nil, errAssertion
+	}
+
+	return req, nil
+}
+
+func encodeGetAllResponse(_ context.Context, response interface{}) (interface{}, error) {
+	res, err := response.(*pb.Response)
+
+	if err {
+		return nil, errAssertion
+	}
+
+	return res, nil
 }
